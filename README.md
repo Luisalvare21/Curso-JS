c2ae502e52d8264bc1275bba0f5eb88765654c8f
luisalvare22 <luisalvarezcorpha@gmail.com>
2021-12-13 11:29:21 -0500

    Curso Javascript

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Curso-JS
+# Curso-Práctico-de-JavaScript
+
+## Practicando un poco de JS :)
+
+### Modulos:
+
+#### Figuras Geométricas
+  - Primer paso: Definir las fórmulas
+  - Segundo paso: Implementar las fórmulas en Javascript
+  - Tercer paso: Crear Funciones
+  - Cuarto paso: Integrar Js con HTML
+
+#### Resultado
+
+![Figuras Geometricas](https://raw.githubusercontent.com/iJCode1/Curso-Practico-de-JavaScript/main/assets/images/captura2.png)
+
+
+
+#### Precios & Descuentos
+  - Primer paso: Definir las fórmulas
+  - Segundo paso: Implementar las fórmulas en Javascript
+  - Tercer paso: Crear Funciones
+  - Cuarto paso: Integrar Js con HTML
+
+#### Resultado
+
+![Precios y Descuentos](https://raw.githubusercontent.com/iJCode1/Curso-Practico-de-JavaScript/main/assets/images/descuentos.png)
+
+
---
+++ js/salarios.js
@@ -0,0 +1,102 @@
+/*
+    Agregando los salarios de México
+*/
+
+const salariosColombia = [];
+
+salariosColombia.push({
+    name: "Joel",
+    salary: 37000
+});
+salariosColombia.push({
+    name: "Julieta",
+    salary: 28434
+});
+salariosColombia.push({
+    name: "Keyla",
+    salary: 34274
+});
+salariosColombia.push({
+    name: "Samuel",
+    salary: 18364
+});
+salariosColombia.push({
+    name: "Elias",
+    salary: 18700
+});
+salariosColombia.push({
+    name: "Jose",
+    salary: 12400
+});
+salariosColombia.push({
+    name: "Andrea",
+    salary: 16560
+});
+salariosColombia.push({
+    name: "Carmen",
+    salary: 18373
+});
+salariosColombia.push({
+    name: "Carlos",
+    salary: 19373
+});
+salariosColombia.push({
+    name: "Agustin",
+    salary: 17823
+});
+salariosColombia.push({
+    name: "Enrique",
+    salary: 19734
+});
+salariosColombia.push({
+    name: "Lizbeth",
+    salary: 27473
+});
+salariosColombia.push({
+    name: "Cesar",
+    salary: 19372
+});
+salariosColombia.push({
+    name: "Oscar",
+    salary: 10834
+});
+salariosColombia.push({
+    name: "Antonio",
+    salary: 16723
+});
+salariosColombia.push({
+    name: "Camila",
+    salary: 18947
+});
+salariosColombia.push({
+    name: "Carla",
+    salary: 36427
+});
+salariosColombia.push({
+    name: "Esmeralda",
+    salary: 19374
+});
+salariosColombia.push({
+    name: "Pepe",
+    salary: 36821
+});
+salariosColombia.push({
+    name: "Monica",
+    salary: 36482
+});
+salariosColombia.push({
+    name: "Erick",
+    salary: 26483
+});
+salariosColombia.push({
+    name: "Rosa",
+    salary: 18492
+});
+salariosColombia.push({
+    name: "Marisela",
+    salary: 46283
+});
+salariosColombia.push({
+    name: "Martha",
+    salary: 25372
+});
